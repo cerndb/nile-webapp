00932cef9ed009c335fddc0cf07250bbded73d22
Alicia de Dios <alicia.de.dios.fuentes@cern.ch>
2017-03-10 16:30:02 +0100

    Included general CERN tool bar

---
+++ src/app/theme/elements/cern-toolbar/cern-toolbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CernToolbarComponent } from './cern-toolbar.component';
+
+describe('CernToolbarComponent', () => {
+  let component: CernToolbarComponent;
+  let fixture: ComponentFixture<CernToolbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CernToolbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CernToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/theme/elements/cern-toolbar/cern-toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cern-toolbar',
+  templateUrl: './cern-toolbar.component.html',
+  styleUrls: ['./cern-toolbar.component.css']
+})
+export class CernToolbarComponent {
+
+  constructor() { }
+
+}
