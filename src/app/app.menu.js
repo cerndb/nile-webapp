1048c81040d8b687df53210256c6eaf00cc9fb16
Alicia de Dios <alicia.de.dios.fuentes@cern.ch>
2017-03-22 15:41:50 +0100

    Exclude all typescript compiled files

--- src/app/app.menu.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Created by adediosf on 16/02/17.
- */
-var pages_menu_1 = require("./pages/pages.menu");
-exports.MENU = pages_menu_1.PAGES_MENU.slice();
--- src/app/theme/elements/side-bar/menu/menu-item/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-/**
- * Created by adediosf on 07/03/17.
- */
-__export(require("./menu-item.component.ts"));
